a5dbaf536c0cb27b843a65bd42df25bb54eb86a8
Phan Nghia <79625811+phannghia2310@users.noreply.github.com>
2024-07-11 12:30:53 +0700

    Modified appsettings.json

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edd223aea68c008302b34ccafc15cc3aeec2947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b12b2fc72dbdf5dba0f2882478ddec728f5c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
