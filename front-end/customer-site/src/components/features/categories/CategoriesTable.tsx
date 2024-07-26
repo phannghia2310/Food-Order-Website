3878be99c185d41e54861c1b7e3399c838df2028
Phan Nghia <79625811+phannghia2310@users.noreply.github.com>
2024-07-26 20:42:20 +0700

    Update customer site

--- front-end/customer-site/src/components/features/categories/CategoriesTable.tsx
+++ front-end/customer-site/src/components/features/categories/CategoriesTable.tsx
@@ -1,7 +1,16 @@
 import { PencilSquareIcon } from "@/icons/PencilSquareIcon";
 import { TrashIcon } from "@/icons/TrashIcon";
 import Category from "@/types/Category";
-import { Avatar, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip } from "@nextui-org/react";
+import {
+  Avatar,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Tooltip,
+} from "@nextui-org/react";
 import { useState } from "react";
 import ModalContainer from "../../common/ModalContainer";
 
@@ -11,18 +20,26 @@ interface CategoriesTableProps {
   categories: Category[];
 }
 
-const CategoriesTable = ({ onEdit, onDelete, categories }: CategoriesTableProps) => {
+const CategoriesTable = ({
+  onEdit,
+  onDelete,
+  categories,
+}: CategoriesTableProps) => {
   const [openModals, setOpenModals] = useState<{ [key: string]: boolean }>({});
 
   function handleOpenChange(category: Category, openState: boolean): void {
     setOpenModals((prevOpenModals) => ({
       ...prevOpenModals,
-      [category._id]: openState,
+      [category.categoryId]: openState,
     }));
   }
 
   return (
-    <Table aria-label="Categories Table" isStriped classNames={{ th: "text-md", td: "text-md text-gray-300" }}>
+    <Table
+      aria-label="Categories Table"
+      isStriped
+      classNames={{ th: "text-md", td: "text-md text-gray-300" }}
+    >
       <TableHeader>
         <TableColumn>Image</TableColumn>
         <TableColumn>Category Name</TableColumn>
@@ -31,27 +48,41 @@ const CategoriesTable = ({ onEdit, onDelete, categories }: CategoriesTableProps)
       {categories.length > 0 ? (
         <TableBody>
           {categories.map((category) => (
-            <TableRow key={category._id}>
+            <TableRow key={category.categoryId}>
               <TableCell>
-                <Avatar src={category.image} radius="md" className="bg-transparent w-[140px] h-auto" />
+                <Avatar
+                  src={""}
+                  radius="md"
+                  className="bg-transparent w-[140px] h-auto"
+                />
               </TableCell>
-              <TableCell>{category.name}</TableCell>
+              <TableCell>{category.categoryName}</TableCell>
               <TableCell>
                 <div className="relative flex items-center gap-2">
                   <Tooltip content="Edit category">
-                    <span className="text-lg cursor-pointer active:opacity-50" onClick={() => onEdit(category)}>
+                    <span
+                      className="text-lg cursor-pointer active:opacity-50"
+                      onClick={() => onEdit(category)}
+                    >
                       <PencilSquareIcon className={"w-6"} />
                     </span>
                   </Tooltip>
                   <Tooltip color="danger" content="Delete category">
-                    <span className="text-lg text-danger cursor-pointer active:opacity-50" onClick={() => handleOpenChange(category, true)}>
+                    <span
+                      className="text-lg text-danger cursor-pointer active:opacity-50"
+                      onClick={() => handleOpenChange(category, true)}
+                    >
                       <TrashIcon className={"w-6"} />
                       <ModalContainer
-                        isOpen={openModals[category._id]}
-                        title={`Delete category ${category.name}?`}
-                        content={"Are you sure you want to delete this category?"}
+                        isOpen={openModals[category.categoryId]}
+                        title={`Delete category ${category.categoryName}?`}
+                        content={
+                          "Are you sure you want to delete this category?"
+                        }
                         confirmText={"Yes, delete it"}
-                        onConfirm={() => { onDelete(category), handleOpenChange(category, false) }}
+                        onConfirm={() => {
+                          onDelete(category), handleOpenChange(category, false);
+                        }}
                         closeText="Cancel"
                         onClose={() => handleOpenChange(category, false)}
                       />
@@ -66,7 +97,7 @@ const CategoriesTable = ({ onEdit, onDelete, categories }: CategoriesTableProps)
         <TableBody emptyContent={"No categories to display"}>{[]}</TableBody>
       )}
     </Table>
-  )
-}
+  );
+};
 
-export default CategoriesTable
+export default CategoriesTable;
--- front-end/customer-site/src/components/features/categories/CategoryTag.tsx
+++ front-end/customer-site/src/components/features/categories/CategoryTag.tsx
@@ -1,16 +1,25 @@
-import { Button } from '@nextui-org/react'
+import { Button } from "@nextui-org/react";
 
 interface CategoryTagProps {
-  name: string,
-  onClick: (name: string) => void,
-  isSelected: boolean
+  name: string;
+  onClick: (name: string) => void;
+  isSelected: boolean;
 }
 
 const CategoryTag = ({ name, onClick, isSelected }: CategoryTagProps) => {
-  const buttonStyle = isSelected ? 'bg-primary border-light text-dark' : 'bg-dark border-primary text-primary hover:bg-primary hover:border-light hover:text-dark'
+  const buttonStyle = isSelected
+    ? "bg-primary border-light text-dark"
+    : "bg-dark border-primary text-primary hover:bg-primary hover:border-light hover:text-dark";
   return (
-    <Button size='lg' radius='full' className={`border-2 ${buttonStyle}`} onClick={()=>onClick(name)}>{ name}</Button>
-  )
-}
+    <Button
+      size="lg"
+      radius="full"
+      className={`border-2 ${buttonStyle}`}
+      onClick={() => onClick(name)}
+    >
+      {name}
+    </Button>
+  );
+};
 
-export default CategoryTag
+export default CategoryTag;
